67e095d51103d6d218b01c9cf3d0a55f93e0ac02
Oltion-Koceku <koceku17@gmail.com>
2024-03-13 15:13:36 +0100

    [add]barra

--- js/script.js
+++ js/script.js
@@ -3,18 +3,29 @@ const { createApp }=Vue;
 createApp({
   data(){
     return{
-      spesa:{
-        text: "Comprare Latte e Biscotti",
-        done: false
-      },
-      palestra:{
-        text: "Completare la scheda di oggi",
-        done: false
-      },
-      corsa:{
-        text: "Fare 10km di corsa",
-        done: false
-      },
+      tasks:{
+        spesa:{
+          text: "Comprare Latte e Biscotti",
+          done: true
+        },
+        palestra:{
+          text: "Completare la scheda di oggi",
+          done: false
+        },
+        corsa:{
+          text: "Fare 10km di corsa",
+          done: false
+        }
+      }
+    }
+  },
+
+  methods:{
+    remuveOJ(indice){
+      console.log(indice);
+      // if (this.done) {
+        this.tasks.delete(indice, 1)
+      // }
     }
   }
 }).mount("#app");
